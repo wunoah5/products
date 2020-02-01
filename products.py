822bb50cc24299189a7a245fad53707374bc4e47
Noah5 <wu.noah5@gmail.com>
2020-02-01 21:41:43 +0800

    # v0.2 建立檔案寫入

--- products.py
+++ products.py
@@ -1,12 +1,22 @@
 # 記帳程式
 # v0.1 紀錄消費項目跟金額
+# v0.2 建立檔案寫入
+
 products = []
 while True:
     name = input('請輸入消費項目: ')
     if name == 'q':
         break
-    price =  input('請書 入金額: ')
+    price =  input('請輸入金額: ')
     products.append([name, price])
-print(products)
+print('本次輸入資料為')
 
-	
+#這邊在提醒本次輸入的資料
+for p in products:
+	print('消費項目', p[0], '消費金額', p[1])
+
+# 把檔案存下
+with open('products.txt', 'w') as f:
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')    # 在最後面加上\n表示每一行為一筆資料
+print('資料已寫入')
